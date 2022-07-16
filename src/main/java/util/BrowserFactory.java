0dfabfda110b193abe89efe85cedb7cfc2bed39f
Md Sirajus Salekin <salekin.buet04@Salekin.eng-alliance>
2022-07-15 19:52:30 -0500

    commited

---
+++ src/main/java/util/BrowserFactory.java
@@ -0,0 +1,27 @@
+package util;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserFactory {	
+	
+	static WebDriver driver;
+
+public static WebDriver startBrowser() {
+	System.setProperty("webdriver.chrome.driver", "C:\\NOSHIN\\Cucumber_Projects\\AC\\automat\\automat\\homwork\\Drivers\\chromedriver.exe");
+	driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().deleteAllCookies();
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	driver.get("http://techfios.com/test/101/");
+	return driver;
+}
+
+
+
+
+
+
+}
